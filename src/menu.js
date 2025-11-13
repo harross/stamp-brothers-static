aea90a91464a2a35f8a7491b2c80fcf21fd8882d
harross <har.ross@proton.me>
2025-11-13 12:02:07 +0000

    JS added for disabling scroll during menu

--- src/menu.js
+++ src/menu.js
@@ -3,7 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const menu = document.getElementById("menu");
 
   toggle.addEventListener("click", () => {
-    menu.classList.toggle("-translate-y-full");
-    menu.classList.toggle("pointer-events-none");
+    menu.classList.toggle("-translate-y-full"); // Pull menu down into view
+    menu.classList.toggle("pointer-events-none"); // Stop cursor events when menu active
+    menu.classList.toggle("remove-scrolling"); // Remove scroll when menu active
   });
 });
+
+function removeScrolling() {
+  document.body.classList.add("remove-scrolling");
+}
